3b91bfb8357b27efc94d3c743fa3303856f54c32
kuba018 <03bednarczyk@wp.pl>
2025-11-26 23:08:45 +0100

    przygotowania aplikacji do deplymentu

--- frontend/src/components/info.vue
+++ frontend/src/components/info.vue
@@ -1,29 +1,37 @@
 <template>
   <section class="info-panel">
-    <h2 class="info-title">Informacja</h2>
 
     <div class="info-list">
 
       <article class="info-card">
-        <h3 class="info-card-title">Szybkie sprawdzenie działki</h3>
+        <h3 class="info-card-title">Pierwsze kroki:</h3>
         <p class="info-card-text">
-          Po udanej rejestracji, należy zweryfikować swój adres email poprzez link aktywacyjny wysłany na wskazaną skrzynke pocztową.
+          Po rejestracji wymagane jest potwierdzenie adresu email poprzez link aktywacyjny
+          wysłany na podaną skrzynkę pocztową. Do logowania możesz użyć zarówno nazwy użytkownika,
+          jak i zarejestrowanego adresu email. Jeśli zapomnisz hasła, dostępna jest możliwość
+          jego zresetowania za pomocą linku przesłanego na twoją pocztę.
         </p>
       </article>
 
       <article class="info-card">
-        <h3 class="info-card-title">Na podstawie danych ewidencyjnych</h3>
+        <h3 class="info-card-title">Panel użytkownika... i co dalej?</h3>
         <p class="info-card-text">
-          Możesz wyjaśnić skąd pochodzą dane i jakie informacje są wykorzystywane.
+          Po udanym zalogowaniu, w zakładce "Działki", (dostępnej jedynie dla zweryfikowanych
+          emailem użytkowników) możesz wyszukać swoją działkę po unikalnym numerze identyfikacyjnym.
+          Aby znaleźć ten numer, musisz udać się na stronę 
+          <a href="https://www.geoportal.gov.pl" target="_blank" rel="noopener noreferrer">geoportal.gov.pl</a>,
+          <a href="https://geoportal360.pl" target="_blank" rel="noopener noreferrer">geoportal360.pl</a>
+          lub inny serwis świadczący usługę identyfikacji działek.
+          <br>
+          Po udanym pobraniu informacji na temat twojej działki, możesz jej przypisać unikalne
+          kryteria podatkowe, definiując zajmowane pole powierzchni oraz stawki podatkowe.
+          Aby znaleźć szczegółowe zasady liczenia podatku twojej działki,
+          musisz sprawdzić uchwałę rady swojej gminy, ponieważ tylko tam jest to zdefiniowane.
         </p>
       </article>
 
       <article class="info-card">
-        <h3 class="info-card-title">Dlaczego to jest przydatne</h3>
-        <p class="info-card-text">
-          Opiszesz komu narzędzie pomaga: osobom prywatnym,
-          doradcom podatkowym, gminom itp.
-        </p>
+        <h3 class="info-card-title">Życzę miłego użytkowania strony!</h3>
       </article>
 
     </div>
